fe69a223d414c341b462920a58c39e7c140a9991
Qiaozheng Liu <qiaozheng.liu@pax.us>
2025-02-21 16:07:30 -0500

    Add --command, --help. Call api based on commands.

--- apiTool/src/main/java/com/paxus/paxstore/api/tool/App.java
+++ apiTool/src/main/java/com/paxus/paxstore/api/tool/App.java
@@ -1,9 +1,13 @@
 package com.paxus.paxstore.api.tool;
 
+import static com.pax.market.api.sdk.java.api.constant.Constants.APP_TYPE_NORMAL;
+import static com.pax.market.api.sdk.java.api.constant.Constants.APP_TYPE_PARAMETER;
+
 import com.pax.market.api.sdk.java.api.base.dto.AppDetailDTO;
 import com.pax.market.api.sdk.java.api.base.dto.Result;
 import com.pax.market.api.sdk.java.api.developer.DeveloperApi;
 import com.pax.market.api.sdk.java.api.developer.dto.CreateApkRequest;
+import com.pax.market.api.sdk.java.api.io.UploadedFileContent;
 import com.pax.market.api.sdk.java.api.util.FileUtils;
 
 import org.apache.commons.cli.CommandLine;
@@ -16,10 +20,20 @@ import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
     public static final Logger logger = LoggerFactory.getLogger(App.class);
-    public static final String releaseFolder = "release-folder";
+    public static final String releaseFolderPath = "release-folder.zip";
+    public static final String cfgFolderPath = "cfg";
+    public static final String[] commands = new String[]{"main", "getAppInfo", "uploadApk"};
+
+    public static String apiKey, apiSecret, apiUrl, appName, pkgName, command;
+    static DeveloperApi developerApi;
 
     public static void main(String[] args) {
         /**
@@ -29,13 +43,11 @@ public class App {
          * [--url] url: global paxstore, github secret?
          * [--app] app name:
          * [--package] package name:
-
          * required configs when upload:
          * [paths]
          * - apk
          * - release note
          * - param template
-
          * optional configs when upload:
          * [paths]
          * - screenshot
@@ -63,65 +75,175 @@ public class App {
 
         // options
         Options options = new Options();
-        Option keyOption = createOption("k", "key", "API_KEY", "Paxstore developer API key", true);
-        Option secretOption = createOption("s", "secret", "API_SECRET", "Paxstore developer API secret", true);
-        Option urlOption = createOption("u", "url", "API_URL", "Paxstore developer API url", true);
-        Option appOption = createOption("a", "appname", "APP_NAME", "application name", true);
-        Option pkgOption = createOption("p", "pkgname", "PACKAGE_NAME", "application package name", true);
+        Option keyOption = createOption("k", "key", "API_KEY", "paxstore developer API key", true, true);
+        Option secretOption = createOption("s", "secret", "API_SECRET", "paxstore developer API secret", true, true);
+        Option urlOption = createOption("u", "url", "API_URL", "paxstore developer API url", true, true);
+        Option appOption = createOption("a", "appname", "APP_NAME", "application name", true, true);
+        Option pkgOption = createOption("p", "pkgname", "PACKAGE_NAME", "application package name", true, true);
+        Option commandOption = createOption("c", "command", "COMMAND", "available commands: " + Arrays.toString(commands), true, true);
+        Option helpOption = createOption("h", "help", "", "show help message", false, false);
         options.addOption(keyOption)
                 .addOption(secretOption)
                 .addOption(urlOption)
                 .addOption(appOption)
-                .addOption(pkgOption);
-
-//        // print help info
-//        HelpFormatter helpFormatter = new HelpFormatter();
-//        options.addOption("?", "help", false, "Display help information");
-//        helpFormatter.printHelp("apiTool -k apiKey -s apiSecret -u apiUrl -a appName -p packageName", options);
+                .addOption(pkgOption)
+                .addOption(commandOption)
+                .addOption(helpOption);
 
         // parse args
-        String apiKey, apiSecret, apiUrl, appName, pkgName;
         try {
             CommandLineParser commandLineParser = new DefaultParser();
+            HelpFormatter formatter = new HelpFormatter();
             CommandLine cmd = commandLineParser.parse(options, args);
+            // TODO: only -h should also work
+            if (cmd.hasOption(helpOption)) {
+                formatter.printHelp("paxstore api tool", options);
+                return;
+            }
+
             apiKey = cmd.getOptionValue(keyOption);
             apiSecret = cmd.getOptionValue(secretOption);
             apiUrl = cmd.getOptionValue(urlOption);
             appName = cmd.getOptionValue(appOption);
             pkgName = cmd.getOptionValue(pkgOption);
+            command = cmd.getOptionValue(commandOption);
         } catch (ParseException e) {
             logger.error(e.getMessage());
             return;
         }
 
         // developer api
-        DeveloperApi developerApi = new DeveloperApi(apiUrl, apiKey, apiSecret);
-
-        // getAppInfo and check if app exists
-        boolean exist = false;
-        Result<AppDetailDTO> appInfo = getAppInfoByName(developerApi, pkgName, appName);
-        if (appInfo.getBusinessCode() != 0) {
-            logger.error("get app info failed. error code: " + appInfo.getBusinessCode());
-            return;
-        } else if (appInfo.getData().getPackageName() == null) {
-            logger.info(pkgName + " doesn't exist on PAXSTORE. Create new application.");
-        } else {
-            logger.info(pkgName + " exists on PAXSTORE. Upload new version.");
-            exist = true;
+        developerApi = new DeveloperApi(apiUrl, apiKey, apiSecret);
+        switch (command) {
+            case "main":
+                executeMain();
+                break;
+            case "getAppInfo":
+                executeGetAppInfo();
+                break;
+            case "uploadApk":
+                executeUploadApk();
+                break;
+            default:
+                logger.error("Not a valid command. Available commands: " + Arrays.toString(commands));
         }
     }
 
-    public static Option createOption(String shortName, String longName, String argName, String description, boolean required) {
+    public static void executeMain() {
+        // getAppInfo and check if app exists
+        boolean appExist = false;
+        AppDetailDTO appInfo = getAppInfoByName();
+        if (appInfo == null) {
+            return;
+        } else {
+            appExist = (appInfo.getPackageName() != null);
+        }
+
+        // upload apk
+    }
+
+    public static void executeGetAppInfo() {
+        AppDetailDTO data = getAppInfoByName();
+        if (data != null) {
+            logger.info(String.format("\nid: %s\ntype: %s\nos type: %s\nstatus: %s", data.getId(), data.getType(), data.getOsType(), data.getStatus()));
+        }
+    }
+
+    public static void executeUploadApk() {
+
+    }
+
+    public static Option createOption(String shortName, String longName, String argName, String description, boolean hasArg, boolean required) {
         return Option.builder(shortName)
                 .longOpt(longName)
                 .argName(argName)
                 .desc(description)
-                .hasArg()
+                .hasArg(hasArg)
                 .required(required)
                 .build();
     }
 
-    public static Result<AppDetailDTO> getAppInfoByName(DeveloperApi developerApi, String packageName, String appName) {
-        return developerApi.getAppInfoByName(packageName, appName);
+    public static AppDetailDTO getAppInfoByName() {
+        Result<AppDetailDTO> appInfo = developerApi.getAppInfoByName(pkgName, appName);
+        if (appInfo == null) {
+            logger.error("get app info by name error.");
+            return null;
+        } else if (appInfo.getBusinessCode() != 0) {
+            logger.error("get app info failed. error code: " + appInfo.getBusinessCode());
+            return null;
+        } else if (appInfo.getData().getPackageName() == null) {
+            logger.info(pkgName + " doesn't exist on PAXSTORE.");
+        } else {
+            logger.info(pkgName + " exists on PAXSTORE.");
+        }
+        return appInfo.getData();
+    }
+
+    public static Result<String> uploadApk(DeveloperApi developerApi, String appName, String pkgName) throws IOException {
+        // prepare unzip release folder
+        File zipFile = new File(releaseFolderPath);
+        String destDir = zipFile.getParent(); // Extract in the same directory
+        File[] extractedFiles;
+
+        // unzip
+        try {
+            extractedFiles = Utils.unzip(releaseFolderPath); // Unpack
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+        if (extractedFiles.length != 1) {
+            logger.error("unpack the wrong release folder.");
+            return null;
+        }
+
+        // get file path from release folder
+        String apkSuffix = ".apk", releaseNoteSuffix = "ReleaseNote.txt", paramSuffix = ".zip";
+        File releaseFolder = extractedFiles[0];
+        List<String> apkFilePaths = Utils.listAndMatchFile(releaseFolder, apkSuffix);
+        List<String> releaseNoteFilePaths = Utils.listAndMatchFile(releaseFolder, releaseNoteSuffix);
+        List<String> paramFilePaths = Utils.listAndMatchFile(releaseFolder, paramSuffix);
+        // TODO: what about automation? can be more than one apk file collected? Only one app now
+        // TODO: matrix build, release and automation are two 'build' step, but share one 'release' step
+
+        // apk file path, release note, param template list
+        if (apkFilePaths.size() != 1) {
+            logger.error("There should only be one apk file. Found: " + apkFilePaths);
+            return null;
+        }
+        String apkFilePath = apkFilePaths.get(0);
+        if (releaseNoteFilePaths.size() != 1) {
+            logger.error("There should only be one release note file. Found: " + releaseNoteFilePaths);
+            return null;
+        }
+        String releaseNote = Utils.readFileToString(releaseNoteFilePaths.get(0));
+        String baseType = pkgName.contains("manager") ? APP_TYPE_NORMAL : APP_TYPE_PARAMETER;
+        if (paramFilePaths.size() == 0 && baseType.equals(APP_TYPE_PARAMETER)) {
+            logger.error("Parameter app but no param file found.");
+            return null;
+        }
+        List<UploadedFileContent> paramTemplateList = paramFilePaths.stream()
+                .map(FileUtils::createUploadFile)
+                .collect(Collectors.toList());
+
+        CreateApkRequest createApkRequest = new CreateApkRequest();
+//        createApkRequest.setAppFile(FileUtils.createUploadFile(apkFilePath));
+//        createApkRequest.setAppName(appName);
+//        createApkRequest.setBaseType(baseType);
+//        createApkRequest.setShortDesc(shortDesc);
+//        createApkRequest.setDescription(fullDesc);
+//        createApkRequest.setReleaseNotes(releaseNote);
+//        createApkRequest.setChargeType(chargeType);
+//
+//        createApkRequest.setCategoryList(categoryList);
+//        createApkRequest.setModelNameList(modelNameList);
+//        createApkRequest.setScreenshotFileList(screenshotList);
+//        createApkRequest.setParamTemplateFileList(paramTemplateList);
+//
+//
+//        createApkRequest.setFeaturedImgFile(FileUtils.createUploadFile(featuredImgFilePath));
+//        createApkRequest.setIconFile(FileUtils.createUploadFile(iconFilePaTH));
+
+        return developerApi.uploadApk(createApkRequest);
     }
 }
---
+++ apiTool/src/main/java/com/paxus/paxstore/api/tool/Utils.java
@@ -0,0 +1,137 @@
+package com.paxus.paxstore.api.tool;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.*;
+
+public class Utils {
+
+    /**
+     * Unzips a given ZIP file into the same directory and returns the extracted files.
+     *
+     * @param zipFilePath Path to the ZIP file.
+     * @return An array of extracted files.
+     * @throws IOException if an I/O error occurs.
+     */
+    public static File[] unzip(String zipFilePath) throws IOException {
+        File zipFile = new File(zipFilePath);
+        String destDir = zipFile.getParent(); // Extract to the same directory
+
+        File destFolder = new File(destDir);
+        if (!destFolder.exists()) destFolder.mkdirs();
+
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                File outFile = new File(destDir, entry.getName());
+                if (entry.isDirectory()) {
+                    outFile.mkdirs();
+                } else {
+                    extractFile(zis, outFile);
+                }
+                zis.closeEntry();
+            }
+        }
+        return destFolder.listFiles((dir, name) -> !name.equals(zipFile.getName())); // Return extracted files
+    }
+
+    /**
+     * Extracts a file from the ZIP input stream.
+     *
+     * @param zis     The ZipInputStream.
+     * @param outFile The file to extract.
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void extractFile(ZipInputStream zis, File outFile) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(outFile)) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+        }
+    }
+
+    /**
+     * Processes the extracted files.
+     *
+     * @param files An array of extracted files.
+     */
+    public static void processFiles(File[] files) {
+        if (files == null) return;
+        for (File file : files) {
+            System.out.println("Processing: " + file.getAbsolutePath());
+            // Add file processing logic here
+        }
+    }
+
+    /**
+     * Deletes the extracted files after use.
+     *
+     * @param files An array of files to delete.
+     */
+    public static void deleteFiles(File[] files) {
+        if (files == null) return;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                deleteFiles(file.listFiles()); // Recursively delete directories
+            }
+            System.out.println("Deleting: " + file.getAbsolutePath());
+            file.delete();
+        }
+    }
+
+    /**
+     * Recursively lists all files in a directory, and match file.
+     * Use suffix to match files: .apk, .zip, .txt
+     * @param folder root folder
+     * @param suffix suffix
+     * @return matched file paths
+     */
+    public static List<String> listAndMatchFile(File folder, String suffix) {
+        List<String> res = new ArrayList<>();
+
+        if (folder == null || !folder.exists()) {
+            App.logger.debug("folder does not exist: " + (folder != null ? folder.getAbsolutePath() : "null"));
+            return res;
+        }
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    res.addAll(listAndMatchFile(file, suffix));
+                } else if (file.getName().endsWith(suffix)) {
+                    String what = "wrong";
+                    // double check the file is correct: use file name or parent name
+                    if (suffix.equals(".apk") && file.getParent().equals("apk")) {
+                        what = "apk";
+                    } else if (suffix.equals(".zip") && file.getParent().equals("paxstore templates")) {
+                        what = "parameter";
+                    } else if (suffix.equals(".txt") && file.getName().equals("ReleaseNote.txt")) {
+                        what = "release note";
+                    }
+                    App.logger.debug("found " + what + " file" + (what.equals("wrong") ? ", pass" : (": " + file.getAbsolutePath())));
+                    if (!what.equals("wrong")) {
+                        res.add(file.getAbsolutePath());
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Read all content in a file to a string.
+     * @param filePath file path
+     * @return string
+     * @throws IOException
+     */
+    public static String readFileToString(String filePath) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        return String.join("\n", lines);
+    }
+}
+
